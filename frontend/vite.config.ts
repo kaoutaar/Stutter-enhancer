b43576a70f1dbab78250b478004acde9d04fcbf3
jsbloo <josh_bloom96@yahoo.co.uk>
2025-04-17 19:18:54 +0100

    frontend working in docker container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  },
   build: {
     outDir: 'dist', // Output directory for the build
   },
